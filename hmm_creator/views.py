06aed7b40ef17a52d3fd1d285126a2dbe1f19ba5
pockerman <a.giavaras@gmail.com>
2020-09-14 17:39:53 +0100

    Fix views

--- hmm_creator/views.py
+++ hmm_creator/views.py
@@ -8,12 +8,20 @@ from django.core.files.storage import FileSystemStorage
 from hmmtuf.settings import REGIONS_FILES_ROOT
 from hmmtuf.settings import HMM_FILES_ROOT
 
+from .forms import HMMFormCreator
 
 def create_hmm_view(request):
 
+    context = {}
     if request.method == 'POST':
+
+        form = HMMFormCreator(template='hmm_creator/create_hmm_view.html',
+                              context=context)
+
+        result = form.check(request=request)
+
         print("Iam posting....")
 
     template = loader.get_template('hmm_creator/create_hmm_view.html')
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(template.render(context, request))
 
