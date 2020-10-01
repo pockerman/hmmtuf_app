3d2d36fb9937466da9fe5920c1cee0e893461354
pockerman <a.giavaras@gmail.com>
2020-10-01 12:07:04 +0100

    Add delete views

--- hmmtuf_home/views.py
+++ hmmtuf_home/views.py
@@ -8,13 +8,74 @@ from django.shortcuts import redirect
 from django.contrib import messages
 
 from hmmtuf.settings import VITERBI_PATHS_FILES_ROOT
-
+from .models import RegionModel
+from .models import HMMModel
 
 # Create your views here.
 def home_view(request):
     template_html = 'hmmtuf_home/index.html'
     template = loader.get_template(template_html)
     return HttpResponse(template.render({}, request))
+
+
+def delete_region_files_view(request):
+    template_html = 'hmmtuf_home/delete_region_files_view.html'
+    context = {}
+    if request.method == 'POST':
+
+        models = RegionModel.objects.all()
+        counter = 0
+        for model in models:
+
+            filename = model.file_region.name
+            try:
+
+                os.remove(filename)
+                model.delete()
+                counter += 1
+            except Exception as e:
+                messages.error(request, "Attempt to remove region "
+                                        "file {0} failed".format(filename))
+                return redirect("index")
+
+        if counter == 0:
+            messages.info(request, "No region files to remove")
+        else:
+            messages.info(request, "Successfully removed {0} region files".format(counter))
+        return redirect("index")
+
+    template = loader.get_template(template_html)
+    return HttpResponse(template.render(context, request))
+
+
+def delete_hmm_files_view(request):
+    template_html = 'hmmtuf_home/delete_hmm_files_view.html'
+    context = {}
+    if request.method == 'POST':
+
+        models = HMMModel.objects.all()
+        counter = 0
+        for model in models:
+
+            filename = model.file_hmm.name
+            try:
+
+                os.remove(filename)
+                model.delete()
+                counter += 1
+            except Exception as e:
+                messages.error(request, "Attempt to remove HMM "
+                                        "file {0} failed".format(filename))
+                return redirect("index")
+
+        if counter == 0:
+            messages.info(request, "No HMM files to remove")
+        else:
+            messages.info(request, "Successfully removed {0} HMM files".format(counter))
+        return redirect("index")
+
+    template = loader.get_template(template_html)
+    return HttpResponse(template.render(context, request))
 
 
 def delete_task_directories_view(request):
