127d327cb13d991086d496a50029e2bd1198e1f3
Alex <a.giavaras@gmail.com>
2020-11-06 10:19:09 +0000

    Remove buggy code

--- spark_integration/spark_manager.py
+++ spark_integration/spark_manager.py
@@ -4,8 +4,7 @@ class SparkManager(object):
 
     def __init__(self, master_url, app_name):
         self._sc = SparkContext(master_url, app_name)
-        #self._sc.addPyFile("/home/alex/qi3/hmmtuf/compute_engine/")
-
+        
     @property
     def sc(self):
         return self._sc
