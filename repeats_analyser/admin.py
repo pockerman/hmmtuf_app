7ed03229d0bf0a271457c784a68b876310bc5cf5
Alex <a.giavaras@gmail.com>
2021-03-02 13:00:18 +0000

    Update administrator

--- repeats_analyser/admin.py
+++ repeats_analyser/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import DistanceMetricType
 from .models import DistanceSequenceType
 from .models import Repeats
+from .models import RepeatsDistances
 
 
 class DistanceMetricTypeAdmin(admin.ModelAdmin):
@@ -15,10 +16,20 @@ class DistanceSequenceTypeAdmin(admin.ModelAdmin):
 
 
 class RepeatsAdmin(admin.ModelAdmin):
-    fields = ['chromosome',  'start_idx', 'end_idx', 'hmm_state', 'gc', 'repeat_seq',  ]
+    fields = ['chromosome',  'start_idx', 'end_idx', 'hmm_state', 'gc', 'repeat_seq', ]
     list_display = ('chromosome',  'start_idx', 'end_idx', 'hmm_state', 'gc', 'repeat_seq')
+
+
+class RepeatsDistancesAdmin(admin.ModelAdmin):
+    fields = ['chromosome1', 'start_idx_1', 'end_idx_1',
+              'chromosome2', 'start_idx_2', 'end_idx_2',
+              'value', 'metric_type', 'sequence_type', 'is_normalized']
+    list_display = ('chromosome1', 'start_idx_1', 'end_idx_1',
+                    'chromosome2', 'start_idx_2', 'end_idx_2',
+                    'value', 'metric_type', 'sequence_type', 'is_normalized')
 
 
 admin.site.register(DistanceMetricType, DistanceMetricTypeAdmin)
 admin.site.register(DistanceSequenceType, DistanceSequenceTypeAdmin)
 admin.site.register(Repeats, RepeatsAdmin)
+admin.site.register(RepeatsDistances, RepeatsDistancesAdmin)
