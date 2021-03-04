7950c66b58b369fba014bb44711267dd8aa984ab
Alex <a.giavaras@gmail.com>
2021-03-04 12:36:27 +0000

    #46 Update API

--- db/db_connector.py
+++ db/db_connector.py
@@ -1,80 +1,24 @@
+from abc import abstractmethod
 import sqlite3
 from sqlite3 import Error
 
 
-class SQLiteDBConnector(object):
+class DBConnectorBase(object):
 
     TABLES = ['distance_metric_type',
               'distance_sequence_type', 'repeats',
-              'repeats_distances']
+              'repeats_distances', 'repeats_info', 'gquads_info']
 
     @staticmethod
-    def get_table_names():
+    def get_table_names() -> list:
         """
         Returns the table names the DB has
         """
-        return SQLiteDBConnector.TABLES
+        return DBConnectorBase.TABLES
 
-    def __init__(self, db_file):
+    def __init__(self, db_file: str) -> None:
         self._conn = None
         self._db_file = db_file
-
-    def connect(self):
-        """
-        Connect the to the DB
-        :return:
-        """
-        try:
-            self._conn = sqlite3.connect(self._db_file)
-        except Error as e:
-            print(str(e))
-        return self._conn
-
-    def delete_all_tables(self):
-        """
-        Delete all the tables in the DB
-        """
-        for name in SQLiteDBConnector.TABLES:
-            self.delete_table(tbl_name=name)
-
-    def create_all_tables(self):
-
-        sql_distance_metric_type = "CREATE TABLE IF NOT EXISTS distance_metric_type " \
-                                   "(id INTEGER PRIMARY KEY AUTOINCREMENT, " \
-                                   "type TEXT UNIQUE NOT NULL," \
-                                   "short_cut TEXT UNIQUE NOT NULL)"
-
-        self.cursor.execute(sql_distance_metric_type)
-        self._conn.commit()
-
-        sql_distance_sequence_type = "CREATE TABLE IF NOT EXISTS distance_sequence_type " \
-                                     "(id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT UNIQUE NOT NULL)"
-
-        self.cursor.execute(sql_distance_sequence_type)
-        self._conn.commit()
-
-        sql_repeats = "CREATE TABLE IF NOT EXISTS repeats (id INTEGER PRIMARY KEY AUTOINCREMENT, " \
-                      "chromosome TEXT NOT NULL," \
-                      " start_idx INT NOT NULL, end_idx INT NOT NULL," \
-                      "repeat_seq TEXT NOT NULL, hmm_state TEXT, " \
-                      "gc FLOAT)"
-
-        self.cursor.execute(sql_repeats)
-        self._conn.commit()
-
-        sql_repeats_distances = "CREATE TABLE IF NOT EXISTS repeats_distances (chromosome1 TEXT NOT NULL, " \
-                                "start_idx_1 INT NOT NULL, " \
-                                "end_idx_1 INT NOT NULL, " \
-                                "chromosome2 TEXT NOT NULL, " \
-                                "start_idx_2 INT NOT NULL, " \
-                                "end_idx_2 INT NOT NULL, " \
-                                "value FLOAT, " \
-                                "metric_type_id INT NOT NULL, " \
-                                "sequence_type_id INT NOT NULL, " \
-                                "is_normalized INT NOT NULL)"
-
-        self.cursor.execute(sql_repeats_distances)
-        self._conn.commit()
 
     @property
     def cursor(self):
@@ -82,204 +26,205 @@ class SQLiteDBConnector(object):
             raise ValueError("Not Connected to the DB")
         return self._conn.cursor()
 
-    def print_table_column_names(self, table_name):
+    @abstractmethod
+    def connect(self) -> None:
+        """
+        Connect the to the DB
+        """
+        raise NotImplementedError()
 
-        sql = "PRAGMA table_info(%s)" % table_name
-        self.cursor.execute(sql)
-        print(self.cursor.fetchall())
+    @abstractmethod
+    def delete_all_tables(self) -> None:
+        """
+        Delete all the tables in the DB
+        """
+        raise NotImplementedError()
 
-    def fetch_from_distance_metric_type_table_metric(self, metric_type):
+    @abstractmethod
+    def create_all_tables(self) -> None:
+        """
+        Create all the DB tables
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def print_table_column_names(self, table_name: str) -> None:
+        """
+        Print the column names corresponding to the
+        given table name
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def fetch_from_distance_metric_type_table_by_metric(self, metric_type: str) -> tuple:
         """
         Fetch the metric type corresponding to the given metric_type
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT id, type, short_cut from distance_metric_type where type="%s"''' % metric_type
-        cursor.execute(sql)
-        return cursor.fetchone()
-
-    def fetch_from_distance_metric_type_table_by_short_cut(self, short_cut):
+    @abstractmethod
+    def fetch_from_distance_metric_type_table_by_short_cut(self, short_cut: str) -> tuple:
         """
         Fetch the metric type corresponding to the given short cut
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT id, type, short_cut from distance_metric_type where short_cut="%s"''' % short_cut
-        cursor.execute(sql)
-        return cursor.fetchone()
-
-    def fetch_from_distance_metric_type_table_by_id(self, idx):
+    @abstractmethod
+    def fetch_from_distance_metric_type_table_by_id(self, idx: int) -> tuple:
         """
         Fetch the metric type corresponding to the given idx
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT id, type, short_cut from distance_metric_type where id=%s''' % idx
-        cursor.execute(sql)
-        return cursor.fetchone()
-
-    def fetch_from_distance_metric_type_table_all(self):
+    @abstractmethod
+    def fetch_from_distance_metric_type_table_all(self) -> list:
         """
         Fetch all the metric types
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        sql = '''SELECT * FROM distance_metric_type'''
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        rows = cursor.fetchall()
-        return rows
-
-    def fetch_from_distance_sequence_type_table_by_seq_type(self, seq_type):
+    @abstractmethod
+    def fetch_from_distance_sequence_type_table_by_seq_type(self, seq_type: str) -> tuple:
         """
         Fetch the sequence type corresponding to the given seq_type
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT id, type from distance_sequence_type where type="%s"''' % seq_type
-        cursor.execute(sql)
-        return cursor.fetchone()
-
-    def fetch_from_distance_sequence_type_table_by_id(self, idx):
+    @abstractmethod
+    def fetch_from_distance_sequence_type_table_by_id(self, idx: int) -> tuple:
         """
         Fetch the sequence type corresponding to the given idx
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT id, type from distance_sequence_type where id=%s''' % idx
-        cursor.execute(sql)
-        return cursor.fetchone()
-
-    def fetch_from_distance_sequence_type_table_all(self):
+    @abstractmethod
+    def fetch_from_distance_sequence_type_table_all(self) -> list:
         """
         Returns all the rows in the distance_sequence_type table
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT * from distance_sequence_type'''
-        cursor.execute(sql)
-        return cursor.fetchall()
-
-    def fetch_from_repeats_table_by_id(self, idx):
+    @abstractmethod
+    def fetch_from_repeats_table_by_id(self, idx: int) -> tuple:
         """
         Fetch the repeat corresponding to the given idx
         """
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT * from repeats where id=%s''' % idx
-        cursor.execute(sql)
-        return cursor.fetchone()
+        raise NotImplementedError()
 
-    def fetch_from_repeats_table_by_chromosome(self, chromosome):
+    @abstractmethod
+    def fetch_from_repeats_table_by_chromosome(self, chromosome: str) -> list:
         """
         Fetch the repeats from the given chromosome
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT * from repeats where chromosome="%s"''' % chromosome
-        cursor.execute(sql)
-        return cursor.fetchall()
-
-    def fetch_from_repeats_table_by_coordinates(self, start_idx, end_idx):
+    @abstractmethod
+    def fetch_from_repeats_table_by_coordinates(self, start_idx: int, end_idx: int) -> list:
         """
         Fetch the repeats with the given [start_idx, end_idx)
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT * from repeats where start_idx=%s and end_idx=%s''' % (start_idx, end_idx)
-        cursor.execute(sql)
-        return cursor.fetchall()
-
-    def fetch_from_repeats_table_by_hmm_state(self, hmm_state):
+    @abstractmethod
+    def fetch_from_repeats_table_by_hmm_state(self, hmm_state: str) -> list:
         """
         Fetch the repeats with the given HMM state
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT * from repeats where hmm_state="%s"''' % (hmm_state)
-        cursor.execute(sql)
-        return cursor.fetchall()
-
-    def fetch_from_repeats_table_all(self):
+    @abstractmethod
+    def fetch_from_repeats_table_all(self) -> list:
         """
         Fetch all data from the repeats table
         """
+        raise NotImplementedError()
 
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        sql = '''SELECT * from repeats'''
-        cursor.execute(sql)
-        return cursor.fetchall()
+    @abstractmethod
+    def fetch_from_repeats_info_table_by_id(self, idx: int) -> tuple:
+        """
+        Fetch the unique entry in the repeats_info table
+        """
+        raise NotImplementedError()
 
-    def fetch_one(self, sql):
+    @abstractmethod
+    def fetch_from_repeats_info_table_by_chromosome(self, chromosome: str) -> list:
+        """
+        Fetch all data in the repeats_info table corresponding to
+        the given chromosome
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def fetch_from_repeats_info_table_all(self) -> list:
+        """
+        Fetch all data in the repeats_info table
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def fetch_from_gquads_info_table_by_id(self, idx: int) -> tuple:
+        """
+        Fetch the unique entry in the gquads_info table
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def fetch_from_gquads_info_table_by_chromosome(self, chromosome: str) -> list:
+        """
+        Fetch all data in the gquads_info table corresponding to
+        the given chromosome
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def fetch_from_gquads_info_table_all(self) -> list:
+        """
+        Fetch all data in the gquads_info table
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def fetch_one(self, sql: str) -> tuple:
         """
         Execute the given SQL and fetch one result
         """
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        return cursor.fetchone()
+        raise NotImplementedError()
 
-    def fetch_all(self, sql):
+    @abstractmethod
+    def fetch_all(self, sql: str) -> list:
         """
         Execute the given SQL and fetch all results
         """
-        conn = sqlite3.connect(self._db_file)
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        return cursor.fetchall()
+        raise NotImplementedError()
 
+    @abstractmethod
     def execute(self, sql: str, values: tuple) -> None:
         """
         Execute the sql
-        :param sql:
-        :param values:
-        :return:
         """
-        self.cursor.execute(sql, values)
-        self._conn.commit()
+        raise NotImplementedError()
 
-    def create_table(self, table_name, columns):
+    @abstractmethod
+    def create_table(self, table_name: str, columns: list) -> None:
         """
         Create the table with the given name and the given
         columns. The table is create only if it doesn't exist
-        :param table_name:
-        :param columns:
-        :return:
         """
+        raise NotImplementedError()
 
-        if len(columns) == 0:
-            raise ValueError("Empty column names")
-
-        sql = "CREATE TABLE IF NOT EXISTS {0} (".format(table_name)
-        sql += "{0}".format(columns[0])
-
-        for col in range(1, len(columns) - 1):
-            sql += "{0},".format(columns[col])
-
-        if len(columns) > 1:
-            sql += "{0}".format(columns[len(columns) - 1])
-        sql += ")"
-        self.cursor.execute(sql)
-        self._conn.commit()
-
-    def delete_table(self, tbl_name):
+    @abstractmethod
+    def delete_table(self, tbl_name: str) -> None:
         """
         Delete the table with the given name
         if the table exists
         """
-        sql = "DROP TABLE IF EXISTS {0};".format(tbl_name)
-        self.cursor.execute(sql)
-        self._conn.commit()
+        raise NotImplementedError()
+
+
+
+
+
+
 
 
 
